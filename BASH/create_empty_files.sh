8fb8bcbc0f3df546aacc904599b8674c9f643e58
lucid <euclid@keemail.me>
2021-04-01 00:16:31 +0200

    added json

--- BASH/create_empty_files.sh
+++ BASH/create_empty_files.sh
@@ -2,7 +2,7 @@
 
 mkdir "empty_files"
 cd "empty_files"
-FILE_EXT=(doc docx gif flac mkv pdf pps ppsx ppt pptx xls xlsx)
+FILE_EXT=(doc docx gif json flac mkv pdf pps ppsx ppt pptx xls xlsx)
 for ext in "${FILE_EXT[@]}"
 do
 	touch "$ext.$ext"
